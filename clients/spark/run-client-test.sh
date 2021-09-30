b528a123fbaa72e167563dbc6fac2ad091d24c4d
guyhardonag <guy.hardonag@treeverse.io>
2021-09-30 11:37:29 +0300

    test workflow

---
+++ clients/spark/run-client-test.sh
@@ -0,0 +1,11 @@
+
+# Run Export
+docker-compose run -T -v  $PWD/target/:/target/ -T --no-deps --rm spark-submit bash -c 'spark-submit  --master spark://spark:7077 --conf spark.hadoop.lakefs.api.url=http:/10.5.0.55:8000/api/v1   --conf spark.hadoop.lakefs.api.access_key=${TESTER_ACCESS_KEY_ID}   --conf spark.hadoop.fs.s3a.connection.ssl.enabled=false   --conf spark.hadoop.lakefs.api.secret_key=${TESTER_SECRET_ACCESS_KEY}   --class io.treeverse.clients.Main  /target/lakefs-spark-client-301-assembly-0.1.5.jar test-data s3://nessie-system-testing/export-one-time-test-guy   --branch=main'
+
+# Validate export #TODO add validation
+
+# Run Garbage Collection 
+docker-compose run -T -v  $PWD/target/:/target/ -T --no-deps --rm spark-submit bash -c 'spark-submit --master spark://spark:7077 --class io.treeverse.clients.GarbageCollector   -c spark.hadoop.lakefs.api.url=http:/10.5.0.55:8000/api/v1 -c spark.hadoop.lakefs.api.access_key=${TESTER_ACCESS_KEY_ID}    -c spark.hadoop.lakefs.api.secret_key=${TESTER_SECRET_ACCESS_KEY}     -c spark.hadoop.fs.s3a.access.key=${LAKEFS_BLOCKSTORE_S3_CREDENTIALS_ACCESS_KEY_ID}   -c spark.hadoop.fs.s3a.secret.key=${LAKEFS_BLOCKSTORE_S3_CREDENTIALS_SECRET_ACCESS_KEY} /target/lakefs-spark-client-301-assembly-0.1.5.jar test-data us-east-1'
+
+
+# Validate Garbage Collection #TODO add validation
